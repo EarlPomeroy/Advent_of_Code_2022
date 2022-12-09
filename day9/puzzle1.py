fca3e34c0b22da872c7315fa7dfbd1646428a38c
Earl Pomeroy <earl.pomeroy42@gmail.com>
2022-12-09 16:24:30 -0700

    Day 9 Challenge complete
    Simplified first puzzle code.
    Ugggghhhh!!!! Getting the rope tail on the second problem was a bit of a challenge.

--- day9/puzzle1.py
+++ day9/puzzle1.py
@@ -6,64 +6,59 @@ head_y = 0
 tail_x = 0
 tail_y = 0
 
-
 def move_tail(dir):
     global head_x
     global head_y
     global tail_x
     global tail_y
 
-    # diagnol move
-    if head_x != tail_x and head_y != tail_y and (abs(head_x - tail_x) > 1 or abs(head_y - tail_y) > 1):
-        if dir == "U":
-            tail_y += 1
-            tail_x = head_x
-        elif dir == "D":
-            tail_y -= 1
-            tail_x = head_x
-        elif dir == "R":
-            tail_y = head_y
-            tail_x += 1
-        elif dir == "L":
-            tail_y = head_y
+    # diagonal move
+    if abs(head_x - tail_x) > 1:
+        if tail_x > head_x:
             tail_x -= 1
-    else:
-        if abs(head_x - tail_x) > 1 or abs(head_y - tail_y) > 1:
-            if dir == "U":
-                tail_y += 1
-            elif dir == "D":
-                tail_y -= 1
-            elif dir == "R":
-                tail_x += 1
-            elif dir == "L":
-                tail_x -= 1
+        else:
+            tail_x += 1
 
-    output.append(f"({head_x}, {head_y}) - ({tail_x}, {tail_y}) --")
+        if head_y != tail_y:
+            if tail_y > head_y:
+                tail_y -= 1
+            else:
+                tail_y += 1
+    if abs(head_y - tail_y) > 1:
+        if tail_y > head_y:
+            tail_y -= 1
+        else:
+            tail_y += 1
+        
+        if head_x != tail_x:
+            if tail_x > head_x:
+                tail_x -= 1
+            else:
+                tail_x += 1
+
     if (tail_x, tail_y) not in points:
         points.append((tail_x, tail_y))
-
 
 def move(dir, dist):
     global head_x
     global head_y
 
     if dir == "U":
-        for d in range(1, dist + 1):
+        for d in range(0, dist):
             head_y += 1
             move_tail(dir)
     elif dir == "D":
-        for d in range(1, dist + 1):
+        for d in range(0, dist):
             head_y -= 1
             move_tail(dir)
     elif dir == "R":
-        for d in range(1, dist + 1):
+        for d in range(0, dist):
             head_x += 1
             move_tail(dir)
     elif dir == "L":
-        for d in range(1, dist + 1):
+        for d in range(0, dist):
             head_x -= 1
             move_tail(dir)
-
 
 with open("./input.txt") as fp:
     lines = fp.readlines()
--- day9/puzzle2.py
+++ day9/puzzle2.py
@@ -2,68 +2,64 @@ points = []
 knots_x = [0] * 10
 knots_y = [0] * 10
 
-
-def move_tail(dir, knot_index):
+def move_tail(knot_index):
     global knots_x
     global knots_y
 
     if knot_index > 9:
         return
 
-    # diagnol move
-    if knots_x[knot_index - 1] != knots_x[knot_index] and knots_y[knot_index - 1] != knots_y[knot_index] and (abs(knots_x[knot_index - 1] - knots_x[knot_index]) > 1 or abs(knots_y[knot_index - 1] - knots_y[knot_index]) > 1):
-        if dir == "U":
-            knots_y[knot_index] += 1
-            knots_x[knot_index] = knots_x[knot_index - 1]
-        elif dir == "D":
-            knots_y[knot_index] -= 1
-            knots_x[knot_index] = knots_x[knot_index - 1]
-        elif dir == "R":
-            knots_y[knot_index] = knots_y[knot_index]
-            knots_x[knot_index] += 1
-        elif dir == "L":
-            knots_y[knot_index] = knots_y[knot_index]
+    if abs(knots_x[knot_index - 1] - knots_x[knot_index]) > 1:
+        if knots_x[knot_index] > knots_x[knot_index - 1]:
             knots_x[knot_index] -= 1
-    else:
-        if abs(knots_x[knot_index - 1] - knots_x[knot_index]) > 1 or abs(knots_y[knot_index] - knots_y[knot_index]) > 1:
-            if dir == "U":
-                knots_y[knot_index] += 1
-            elif dir == "D":
+        else:
+            knots_x[knot_index] += 1
+
+        if knots_y[knot_index - 1] != knots_y[knot_index]:
+            if knots_y[knot_index] > knots_y[knot_index - 1]:
                 knots_y[knot_index] -= 1
-            elif dir == "R":
-                knots_x[knot_index] += 1
-            elif dir == "L":
+            else:
+                knots_y[knot_index] += 1
+    if abs(knots_y[knot_index - 1] - knots_y[knot_index]) > 1:
+        if knots_y[knot_index] > knots_y[knot_index - 1]:
+            knots_y[knot_index] -= 1
+        else:
+            knots_y[knot_index] += 1
+        
+        if knots_x[knot_index - 1] != knots_x[knot_index]:
+            if knots_x[knot_index] > knots_x[knot_index - 1]:
                 knots_x[knot_index] -= 1
+            else:
+                knots_x[knot_index] += 1
+
+    move_tail(knot_index + 1)
 
     if knot_index == 9 and (knots_x[knot_index], knots_y[knot_index]) not in points:
         points.append((knots_x[knot_index], knots_y[knot_index]))
-    else:
-        move_tail(dir, knot_index + 1)
-
 
 def move(dir, dist):
     global knots_x
     global knots_y
 
     if dir == "U":
-        for d in range(1, dist + 1):
+        for d in range(0, dist):
             knots_y[0] += 1
-            move_tail(dir, 1)
+            move_tail(1)
     elif dir == "D":
-        for d in range(1, dist + 1):
+        for d in range(0, dist):
             knots_y[0] -= 1
-            move_tail(dir, 1)
+            move_tail(1)
     elif dir == "R":
-        for d in range(1, dist + 1):
+        for d in range(0, dist):
             knots_x[0] += 1
-            move_tail(dir, 1)
+            move_tail(1)
     elif dir == "L":
-        for d in range(1, dist + 1):
+        for d in range(0, dist):
             knots_x[0] -= 1
-            move_tail(dir, 1)
+            move_tail(1)
 
 
-with open("./test.txt") as fp:
+with open("./input.txt") as fp:
     lines = fp.readlines()
     for line in lines:
         dir, dist = line.strip().split(" ")
